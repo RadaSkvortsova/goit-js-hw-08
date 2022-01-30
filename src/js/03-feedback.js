fb241fd74ee529396fde21114e413d90dfbdfec7
HP Probook 4540s <radafirst10@gmail.com>
2022-01-30 17:38:48 +0200

    task 2, 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,49 @@
+import throttle from 'lodash/throttle';
 
+const form = document.querySelector('.feedback-form');
+form.addEventListener('input', throttle(inputText, 500));
+form.addEventListener('submit', submitForm);
+const INPUT_KEY = 'feedback-form-state';
+let objectInput = {};
+
+inForm();
+
+function submitForm(event) {
+  event.preventDefault();
+  const formEl = event.target.elements;
+  const email = formEl.email.value;
+  const message = formEl.message.value;
+  if (message === '' && email === '') {
+    return alert('Поля "email" та "message" мають бути заповнені!');
+  }
+  if (email === '') {
+    return alert('Поле "email" мають бути заповнені!');
+  }
+  if (message === '') {
+    return alert('Поле "message" мають бути заповнені!');
+  }
+  objectInput = {
+    email,
+    message,
+  };
+  console.log(objectInput);
+
+  form.reset();
+  localStorage.removeItem(INPUT_KEY);
+}
+
+function inputText(event) {
+  objectInput[event.target.name] = event.target.value;
+  localStorage.setItem(INPUT_KEY, JSON.stringify(objectInput));
+}
+
+function inForm() {
+  let saveText = localStorage.getItem(INPUT_KEY);
+  if (saveText) {
+    saveText = JSON.parse(saveText);
+    Object.entries(saveText).forEach(([name, value]) => {
+      objectInput[name] = value;
+      form.elements[name].value = value;
+    });
+  }
+}
